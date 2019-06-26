4b33f0802162a6edb517a68f3dbe2fa2dd06f4a3
wpaulnew <wpaulnew@gmail.com>
2019-06-26 12:37:19 +0300

    Third commit

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hi, i see u!'))
+app.get('/', (req, res) => res.send('Application!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
